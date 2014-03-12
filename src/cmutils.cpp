4b49dedb08e1509aaeb6300042d1699be6045755
xy <xy@XYHPC>
2014-03-12 08:41:54 +0800

    Bugs repair: CrossMatch->readStarFile The CrossMatch's method readStarFile does not check the format of a file's line, when the line is blank the program will broken. We repair it by checking the sscanf's return value.

--- src/cmutils.cpp
+++ src/cmutils.cpp
@@ -7,30 +7,67 @@
 #include "cmhead.h"
 
 float getLineDistance(cm_star *p1, cm_star *p2) {
-    float xdiff = p1->pixx - p2->pixx;
-    float ydiff = p1->pixy - p2->pixy;
-    float dist = sqrt(xdiff * xdiff + ydiff * ydiff);
-    return dist;
+  float xdiff = p1->pixx - p2->pixx;
+  float ydiff = p1->pixy - p2->pixy;
+  float dist = sqrt(xdiff * xdiff + ydiff * ydiff);
+  return dist;
+}
+
+/**
+ * judge a string is a blank string, only contain ' ', '\t','\n','\r',eg
+ * @param str
+ * @param len
+ * @return if it is a blank string return true, others return false
+ */
+bool isEmpty(char *str, int len) {
+
+  if (NULL != str && '\0' != str[0]) {
+    for (int i = 0; i < len && '\0' != str[i]; i++) {
+      if (str[i] != ' ' && str[i] != '\t' && str[i] != '\r' && 
+              str[i] != '\n' && str[i] != '\x0b') {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+/**
+ * judge a string whether contain a number char ('0'~"9")
+ * @param str
+ * @param len
+ * @return 
+ */
+bool hasNumber(char *str, int len) {
+  
+  if (NULL != str && '\0' != str[0]) {
+    for (int i = 0; i < len && '\0' != str[i]; i++) {
+      if (str[i] >='0' && str[i] <= '9') {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 template<typename T>
 inline T getMax(const T& a, const T& b) {
-    return a > b ? a : b;
+  return a > b ? a : b;
 }
 
 long countFileLines(char *fName) {
 
-    FILE *fp = fopen(fName, "r");
+  FILE *fp = fopen(fName, "r");
 
-    if (fp == NULL) {
-        return 0;
-    }
+  if (fp == NULL) {
+    return 0;
+  }
 
-    long lineNum = 0;
-    char line[MaxStringLength];
-    while (fgets(line, MaxStringLength, fp) != NULL) {
-        lineNum++;
-    }
-    fclose(fp);
-    return lineNum;
+  long lineNum = 0;
+  char line[MaxStringLength];
+  while (fgets(line, MaxStringLength, fp) != NULL) {
+    lineNum++;
+  }
+  fclose(fp);
+  return lineNum;
 }

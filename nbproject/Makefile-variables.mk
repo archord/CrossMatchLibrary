0f5164d64d5347d2ff619aa7ad6b1e33a363900e
xy <xy@xy780>
2014-12-15 20:35:26 +0800

    switch project from windows to ubuntu.

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -15,13 +15,13 @@ CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=libCrossMatchLibrary.so.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/libCrossMatchLibrary.so.tar
 # Release configuration
-CND_PLATFORM_Release=None-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
-CND_ARTIFACT_NAME_Release=libCrossMatchLibrary.dll
-CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/libCrossMatchLibrary.dll
-CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
-CND_PACKAGE_NAME_Release=libCrossMatchLibrary.dll.tar
-CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/libCrossMatchLibrary.dll.tar
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=libCrossMatchLibrary.so
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/libCrossMatchLibrary.so
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=libCrossMatchLibrary.so.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libCrossMatchLibrary.so.tar
 #
 # include compiler specific variables
 #
